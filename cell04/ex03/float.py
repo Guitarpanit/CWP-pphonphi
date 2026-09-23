2aa83d2cf917492e5f06747aef41c94a578341a6
Guitarpanit <68050349@kmitl.ac.th>
2026-09-23 09:53:39 +0700

    made ex01-ex05 of cell04

---
+++ cell04/ex03/float.py
@@ -0,0 +1,7 @@
+user_input = input("Give me a number: ")
+number  = float(user_input)
+
+if number == int(number):
+    print("The number is an integer.")
+else:
+    print("The number is not a decimal.")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,6 @@
+import math
+
+user_input = input("Give me a number: ")
+number  = float(user_input)
+
+print(math.ceil(number))
