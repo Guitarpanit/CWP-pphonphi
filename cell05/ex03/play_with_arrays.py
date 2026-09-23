09d0d031f2f85bc5b42e1ea19b4fac70842a2bc4
Guitarpanit <68050349@kmitl.ac.th>
2026-09-23 15:59:57 +0700

    Create ex03 and ex04

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,5 @@
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [x+2 for x in array if x > 5]
+new_array = set(new_array)
+print(array)
+print(new_array)
---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,4 @@
+import sys
+
+number_of_params = len(sys.argv) - 1
+print("Number of parameters:", number_of_params, end=".\n")
